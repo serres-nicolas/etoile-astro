bb427cd155e99cb951646fc63a9baa7865900728
Nicolas Serres <miracle@macbook-pro-de-nicolas.home>
2022-10-21 17:01:33 +0200

    first page in developpement

---
+++ src/components/Bouton.jsx
@@ -0,0 +1,13 @@
+import "../styles/Bouton.css";
+
+function Bouton() {
+  return (
+    <div className="boxdl">
+      <a className="dl" href="https://www.facebook.com/EtoileGoussainvilloise/">
+        <img src="/assets/vector.svg" id="two" alt="book logo" class="vector" />
+        <div>Actualités</div>
+      </a>
+    </div>
+  );
+}
+export default Bouton;
